623559d9605f2087f98ce129d4c119a9e4d94a8c
Mikhail Katychev <mkatych@gmail.com>
2020-02-06 18:27:11 -0600

    LONG

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@
 from setuptools import setup, find_packages
 
 try:
-    long_description = open("README.rst").read()
+    long_description = open("README.md").read()
 except IOError:
-    long_description = ""
+    long_description = (
+        "A simple pure python library for the most common GIS functionality."
+    )
 
 setup(
     name="spacey",
-    version="0.1.0",
+    version="0.1.1",
     description="A simple pure python library for the most common GIS functionality.",
     license="MIT",
     author="Mikhail Katychev",
@@ -17,6 +19,7 @@ setup(
     packages=find_packages(),
     install_requires=[],
     long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
