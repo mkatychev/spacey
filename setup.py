15c5292070e721ef1cf9dbd75f9e6004642c67cb
Mikhail Katychev <mkatych@gmail.com>
2020-02-06 18:06:02 -0600

    added setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="spacey",
+    version="0.1.0",
+    description="A simple pure python library for the most common GIS functionality.",
+    license="MIT",
+    author="Mikhail Katychev",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
